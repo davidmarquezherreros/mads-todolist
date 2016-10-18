8fa6f4bc052ca1552e4f10a38df7b7a08620aab7
David Márquez Herreros <davidmarquezherreros@gmail.com>
2016-10-18 09:59:37 +0200

    Realizados 5 Tests para la capa DAO

--- test/dao/UsuarioDaoTest.java
+++ test/dao/UsuarioDaoTest.java
@@ -52,8 +52,53 @@ public class UsuarioDaoTest {
     @Test
     public void buscaUsuarioLogin() {
         jpa.withTransaction(() -> {
-            Usuario usuario = UsuarioDAO.findUsuarioLogin("pepe").get(0);
+            Usuario usuario = UsuarioDAO.findUsuarioLogin("pepe");
             assertNull(usuario);
+        });
+    }
+    // Test para comprobar el funcionamiento de busqueda de un usuario basado en su login + password
+    @Test
+    public void UsuarioLogin(){
+      String parametros = jpa.withTransaction(() ->{
+        Usuario nuevo = new Usuario("david","1234");
+        nuevo = UsuarioDAO.create(nuevo);
+        return nuevo.login+":"+nuevo.password;
+      });
+      jpa.withTransaction(() -> {
+        String[] parts = parametros.split(":");
+        Usuario usuario = UsuarioDAO.Login(parts[0],parts[1]).get(0);
+        assertThat(usuario.login,equalTo("david"));
+      });
+    }
+    // Test para comprobar el funcionamiento de borrado de un usuario
+    @Test
+    public void UsuarioDelete() {
+        Integer id = jpa.withTransaction(() -> {
+            Usuario nuevo = new Usuario("pepe", "pepe");
+            nuevo = UsuarioDAO.create(nuevo);
+            return nuevo.id;
+        });
+
+        jpa.withTransaction(() -> {
+            UsuarioDAO.delete(id);
+            Usuario usuario = UsuarioDAO.find(id);
+            assertNull(usuario);
+        });
+    }
+    // Test para comprobar el funcionamiento de modificar un usuario
+    @Test
+    public void UsuarioUpdate() {
+        Integer id = jpa.withTransaction(() -> {
+            Usuario nuevo = new Usuario("pepe", "pepe");
+            nuevo = UsuarioDAO.create(nuevo);
+            return nuevo.id;
+        });
+
+        jpa.withTransaction(() -> {
+            Usuario modificar = new Usuario("Juan", "1234");
+            Usuario usuario = UsuarioDAO.update(modificar);
+            assertThat(usuario.login,equalTo("Juan"));
+            assertThat(usuario.password,equalTo("1234"));
         });
     }
 }

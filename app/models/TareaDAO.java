6875b7a78843f934dc99440ca8e5cb7937a7ce84
David Márquez Herreros <davidmarquezherreros@gmail.com>
2016-10-20 12:14:14 +0200

    TIC-23 Añadido el codigo para que pasa el test

--- app/models/TareaDAO.java
+++ app/models/TareaDAO.java
@@ -9,4 +9,12 @@ public class TareaDAO {
     public static Tarea find(Integer idTarea) {
         return JPA.em().find(Tarea.class, idTarea);
     }
+    public static Tarea create (Tarea t) {
+        JPA.em().persist(t);
+        // Hacemos un flush y un refresh para asegurarnos de que se realiza
+        // la creación en la BD y se devuelve el id inicializado
+        JPA.em().flush();
+        JPA.em().refresh(t);
+        return t;
+    }
 }
